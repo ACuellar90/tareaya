3f09c0d8bef910170d1c020125fa0df9a1fbf541
ACuellar90 <alexiscuellar2013@gmail.com>
2026-04-28 16:16:26 -0600

    Fix plugin withAndroidManifest

--- plugins/withAndroidManifest.js
+++ plugins/withAndroidManifest.js
@@ -1,13 +1,17 @@
 const { withAndroidManifest } = require('@expo/config-plugins')
 
-module.exports = withAndroidManifest(async (config) => {
-  const mainApplication = config.modResults.manifest.application[0]
-  
-  if (mainApplication['meta-data']) {
-    mainApplication['meta-data'] = mainApplication['meta-data'].filter(
-      item => item.$['android:name'] !== 'com.google.firebase.messaging.default_notification_color'
-    )
-  }
-  
-  return config
-})
+const withFixFirebaseManifest = (config) => {
+  return withAndroidManifest(config, (config) => {
+    const mainApplication = config.modResults.manifest.application[0]
+    
+    if (mainApplication['meta-data']) {
+      mainApplication['meta-data'] = mainApplication['meta-data'].filter(
+        (item) => item.$['android:name'] !== 'com.google.firebase.messaging.default_notification_color'
+      )
+    }
+    
+    return config
+  })
+}
+
+module.exports = withFixFirebaseManifest
